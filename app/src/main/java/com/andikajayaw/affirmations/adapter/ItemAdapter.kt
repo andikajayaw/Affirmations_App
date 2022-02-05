05c64ff6894cf67e4462ed5fd86920b03e657663
andikajayaw <andikajayawiguna@gmail.com>
2022-02-05 22:53:16 +0800

    adding images to the list items

--- app/src/main/java/com/andikajayaw/affirmations/adapter/ItemAdapter.kt
+++ app/src/main/java/com/andikajayaw/affirmations/adapter/ItemAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.andikajayaw.affirmations.R
@@ -19,6 +20,7 @@ class ItemAdapter(private val context: Context, private val dataset: List<Affirm
         // you provide access to all the views for a data item in a view holder.
         // Each data item is just an Affirmation object.
         val textView: TextView = view.findViewById(R.id.item_title)
+        val imageView: ImageView = view.findViewById(R.id.item_image)
     }
 
     /**
@@ -36,6 +38,7 @@ class ItemAdapter(private val context: Context, private val dataset: List<Affirm
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
         holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.imageView.setImageResource(item.imageResourceId)
     }
 
     /**
