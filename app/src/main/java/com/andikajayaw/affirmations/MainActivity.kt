dd9d33f5cfac3445fa892b2a696d2d4f7843a6da
andikajayaw <andikajayawiguna@gmail.com>
2022-02-06 15:26:26 +0800

    change view into grid with 3 item count

--- app/src/main/java/com/andikajayaw/affirmations/MainActivity.kt
+++ app/src/main/java/com/andikajayaw/affirmations/MainActivity.kt
@@ -3,6 +3,7 @@ package com.andikajayaw.affirmations
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.andikajayaw.affirmations.adapter.ItemAdapter
 import com.andikajayaw.affirmations.data.Datasource
@@ -16,7 +17,27 @@ class MainActivity : AppCompatActivity() {
         val myDataSet = Datasource().loadAffirmations()
 
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
-        recyclerView.adapter = ItemAdapter(this, myDataSet)
+
+        /**
+         * CHANGE VISUALIZE INTO GRID LAYOUT MANAGER
+         * */
+        val itemAdapter = ItemAdapter(this, myDataSet)
+
+        recyclerView.apply {
+            layoutManager = GridLayoutManager(this@MainActivity, 3)
+            adapter = itemAdapter
+        }
+        /**
+         * END
+         */
+
+        /**
+         * LINEAR LAYOUT MANAGER - VERTICAL
+         */
+//        recyclerView.adapter = ItemAdapter(this, myDataSet)
+        /**
+         * END
+         */
 
         // Use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
